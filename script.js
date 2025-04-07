5561c921ad3c09bfdf3cd173fcfe3b47854f79cd
Arturo_Davila <arturo.dc182@gmail.com>
2025-04-07 16:47:04 -0600

    Proyecto 2 Comparaciones

--- script.js
+++ script.js
@@ -12,5 +12,25 @@ typeEntry(0);
 typeEntry("");
 typeEntry(null);
 typeEntry(undefined);
-typeEntry(FALSE);
 typeEntry(`asdf`);
+
+let mensaje = function (nota) {
+  if (0 > nota || nota > 100) {
+    console.log("Ingresa un numero entre 0 y 100");
+  } else if (nota >= 90) {
+    console.log("Excelente");
+  } else if (75 <= nota && nota <= 89) {
+    console.log("Bien");
+  } else if (60 <= nota && nota <= 74) {
+    console.log("Suficiente");
+  } else {
+    console.log("No aprobado");
+  }
+};
+
+mensaje(120);
+mensaje(95);
+mensaje(77);
+mensaje(70);
+mensaje(0);
+mensaje(-3);
