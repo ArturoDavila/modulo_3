4051febef3787c2750726e358d6057211574b702
Arturo_Davila <arturo.dc182@gmail.com>
2025-04-07 15:46:18 -0600

    Proyecto 1

---
+++ script.js
@@ -0,0 +1,16 @@
+"strict mode";
+
+let typeEntry = function (data) {
+  console.log(typeof data);
+};
+
+typeEntry(42);
+typeEntry("Veinticinco");
+typeEntry(-666);
+typeEntry(true);
+typeEntry(0);
+typeEntry("");
+typeEntry(null);
+typeEntry(undefined);
+typeEntry(FALSE);
+typeEntry(`asdf`);
